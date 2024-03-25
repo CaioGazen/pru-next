1b2b0250562565681682aba1107e13871c446e5f
Caio Gazen <61031028+CaioGazen@users.noreply.github.com>
2024-03-24 23:57:35 -0300

    better baner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import logo from "../../public/pru-high-resolution-logo.png";
 import "./globals.css";
 import "./normalize.css";
 import "./style.css";
+import { IoHome } from "react-icons/io5";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,31 +23,40 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {" "}
-        <nav className="navbar">
-          <div className="nav-center">
-            <div className="nav-header">
-              <a href="./index.html" className="nav-logo">
-                <Image src={logo} alt="simple starter" />
-              </a>
-              <button type="button" className="btn nav-btn">
-                <i className="fas fa-align-justify"></i>
-              </button>
-            </div>
-            <div className="nav-links">
-              <a href="./" className="nav-link">
-                home
-              </a>
-              <a href="./compra" className="nav-link">
-                Finalizar Compra
-              </a>
-              <div className="nav-link contact-btn">
-                <a href="./login" className="btn">
-                  login
-                </a>
-              </div>
-            </div>
+        <div className="h-32"></div>
+        <div className="fixed top-0 start-0 z-50 flex flex-row h-24 bg-slate-400 shadow-2xl ">
+          <div className="basis-1/12">
+            <a href="./">
+              <Image
+                src={logo}
+                alt=""
+                className="object-contain object-left h-full "
+              />
+            </a>
           </div>
-        </nav>
+          <div className="basis-10/12 flex px-10 justify-start items-center space-x-8">
+            <a
+              href="./"
+              className="p-4 align-middle w-auto h-auto shadow-2xl rounded-3xl bg-slate-300 hover:bg-black hover:text-white hover:rounded-sm transition-all duration-300 ease-linear"
+            >
+              <IoHome />
+            </a>
+            <a
+              href="./compra"
+              className="px-4 py-2 align-middle w-auto h-auto shadow-2xl rounded-3xl bg-slate-300 hover:bg-black hover:text-white hover:rounded-sm transition-all duration-300 ease-linear"
+            >
+              Finalizar Compra
+            </a>
+          </div>
+          <div className="basis-1/12 flex justify-center my-auto mx-auto">
+            <a
+              href="./login"
+              className="px-4 py-2 align-middle w-auto h-auto shadow-2xl rounded-3xl bg-slate-300 hover:bg-black hover:text-white hover:rounded-sm transition-all duration-300 ease-linear"
+            >
+              Fazer login
+            </a>
+          </div>
+        </div>
         {children}
       </body>
     </html>
