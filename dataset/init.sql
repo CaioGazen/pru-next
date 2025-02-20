3be851ff1d1948f5ebe533ac213943e846894504
Caio Gazen <61031028+CaioGazen@users.noreply.github.com>
2025-02-20 00:13:18 -0300

    added initial dataset

--- dataset/init.sql
+++ dataset/init.sql
@@ -10,16 +10,16 @@ CREATE TABLE Pigeons (
 
 -- Example 1: Black pigeon
 INSERT INTO Pigeons (image_path, name, price, pigeon_name)
-VALUES ('images/black.jpg', 'black', 15.00, 'Clóvis');
+VALUES ('/images/black.webp', 'black', 15.00, 'Clóvis');
 
 -- Example 2: Grey pigeon
 INSERT INTO Pigeons (image_path, name, price, pigeon_name)
-VALUES ('images/grey.png', 'grey', 42.00, 'Carlitos');
+VALUES ('/images/default-pigeon.jpg', 'grey', 42.00, 'Carlitos');
 
 -- Example 3: Brown pigeon
 INSERT INTO Pigeons (image_path, name, price, pigeon_name)
-VALUES ('images/brown.jpeg', 'brown', 38.00, 'Cleitin');
+VALUES ('/images/brown.webp', 'brown', 38.00, 'Cleitin');
 
 -- Example 4: White pigeon
 INSERT INTO Pigeons (image_path, name, price, pigeon_name)
-VALUES ('images/white.gif', 'white', 15.00, 'Cléber');
+VALUES ('/images/white.jpg', 'white', 15.00, 'Cléber');
